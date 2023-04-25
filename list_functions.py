55279d5521e6ba6d1d66383c3d0aca91bd9c6486
Liam Gleeson <liamg626@gmail.com>
2023-04-25 13:36:06 +0100

    Add files via upload
    List / array functions in python. Python has no standard array so lists are used instead, unless array is imported in.

---
+++ list_functions.py
@@ -0,0 +1,13 @@
+list = [1, 2, 3]
+
+list.append()	#Adds an element at the end of the list
+list.clear()	#Removes all the elements from the list
+list.copy()	    #Returns a copy of the list
+list.count()	#Returns the number of elements with the specified value
+list.extend()	#Add the elements of a list (or any iterable), to the end of the current list
+list.index()	#Returns the index of the first element with the specified value
+list.insert()	#Adds an element at the specified position
+list.pop()	    #Removes the element at the specified position
+list.remove()	#Removes the first item with the specified value
+list.reverse()	#Reverses the order of the list
+list.sort()	    #Sorts the list
